7784ae6119952bfa130d8dd2fc61e5ce4001f78c
Akash C A <akashca@Akashs-MacBook-Air.local>
2021-04-13 16:35:56 +0530

    Files created till authentication needed to access recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,19 @@
+exit if !Rails.env.development?
+
+puts "Deleting the data"
+Recipe.delete_all
+User.delete_all
+
+puts "Creating User"
+user = FactoryBot.create(:user, email: "test@example.com")
+
+puts "Creating Recipes"
+20.times do
+	FactoryBot.create(:recipe, user: user)
+	
+end
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
